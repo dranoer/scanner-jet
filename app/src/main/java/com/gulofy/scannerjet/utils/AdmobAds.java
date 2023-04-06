0941d91e1338d67a838a00351d994269a1c01279
nazaninnasab <nazanin.nasab@iodigital.com>
2023-04-06 02:49:13 +0200

    add admob IDs

--- app/src/main/java/com/gulofy/scannerjet/utils/AdmobAds.java
+++ app/src/main/java/com/gulofy/scannerjet/utils/AdmobAds.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
